0178699da38051681d959a903dd64f0442c3b415
Haris ur Rehman <hrehman200@gmail.com>
2022-04-26 01:09:53 +0500

    Filtering working on sample data

--- environmental_genomics/views.py
+++ environmental_genomics/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth import authenticate, login
 
 from django.contrib.auth.decorators import login_required
 
@@ -9,19 +10,21 @@ from bugbase.models import Sample, Sample_Data
 @login_required
 def index(request):
     samples = Sample.objects.filter(user=request.user)
+    print(samples)
     return render(request, 'samples.html', {'samples': samples})
 
 
 @login_required
 def sample_data(request, sample_id):
     sample_data = Sample_Data.objects.filter(sample_id=sample_id)
-    kingdoms = Sample_Data.objects.order_by().values('kingdom').distinct()
-    phylums = Sample_Data.objects.order_by().values('phylum').distinct()
-    klass = Sample_Data.objects.order_by().values('klass').distinct()
-    orders = Sample_Data.objects.order_by().values('order').distinct()
-    families = Sample_Data.objects.order_by().values('family').distinct()
-    genus = Sample_Data.objects.order_by().values('genus').distinct()
+    kingdoms = sample_data.values('kingdom').distinct()
+    phylums = sample_data.values('phylum').distinct()
+    klass = sample_data.values('klass').distinct()
+    orders = sample_data.values('order').distinct()
+    families = sample_data.values('family').distinct()
+    genus = sample_data.values('genus').distinct()
     return render(request, 'sample-data.html', {
+        'fluid': 1,
         'sample_data': sample_data,
         'kingdoms': kingdoms,
         'phylums': phylums,
